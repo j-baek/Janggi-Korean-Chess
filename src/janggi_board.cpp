95b4f511693c96c2ce3e42051ff56e5ba70b5b4e
Taeyang Baek <84064951+j-baek@users.noreply.github.com>
2023-11-29 13:26:33 +0000

    janggi board display done

--- src/janggi_board.cpp
+++ src/janggi_board.cpp
@@ -9,17 +9,47 @@ Janggi_Board::Janggi_Board() {
                 board_row.push_back(".");
             }
             board.push_back(board_row);
-        }
+        } 
     }
 
 // Member functions
 //void add_piece(int row, int col, std::str piece);
+
+/*
+    1   2   3   4   5   6   7   8   9 
+   - - - - - - - - - - - - - - - - - - 
+1 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+2 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+3 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+4 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+5 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+6 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+7 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+8 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+9 | p | p | p | p | p | p | p | p | p | 
+   - - - - - - - - - - - - - - - - - - 
+*/
 void Janggi_Board::display_board() {
+    // first display all the row number
+    cout<<"    1     2    3    4    5    6    7    8    9"<<endl;
+    // now display the boarder line
+    cout<<"   - - - - - - - - - - - - - - - - - - - - - - - "<<endl;
+
     for (int i = 0; i < row; i++) {
+        cout<<i+1<<" | ";
         for (int j = 0; j < col; j++){
-            cout<<board[i][j]<<" ";
+            cout<<board[i][j]<<" | ";
         }
         cout<<endl;;
+        cout<<"   - - - - - - - - - - - - - - - - - - - - - - - "<<endl;
     }
 }
 
